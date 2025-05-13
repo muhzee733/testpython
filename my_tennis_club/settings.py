5e26065d6fe69198d701ea8b1a5934554eb2415f
Basitali326 <basatali326@gmail.com>
2025-05-13 17:10:21 +0400

    added

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -112,7 +112,8 @@ USE_I18N = True
 USE_TZ = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://promedicine.frazmedicall.com.au"
+    "promedicine.frazmedicall.com.au",
+    "promedicine.geeklies.xyz"
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
